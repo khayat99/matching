6d748c8069c4b9f022c194781ff7c8897b29ab9f
KHAYAT <mohamed.khayaat@gmail.com>
2022-01-12 13:52:18 +0100

    first commit

---
+++ matching/android/app/src/main/kotlin/com/example/matching/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.matching
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ matching/lib/main.dart
@@ -0,0 +1,83 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Center(child: Text('Image Matching')),
+        backgroundColor: Colors.indigo[800],
+      ),
+      body: ImagePage(),
+    ),
+  ));
+}
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({Key? key}) : super(key: key);
+
+  @override
+  _ImagePageState createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  // var rnd = Random().nextInt(9);
+  int leftImageNumber = 1;
+  int rightImageNumber = 1;
+
+  // ignore: non_constant_identifier_names
+  void ImageRandom() {
+    leftImageNumber = Random().nextInt(8) + 1;
+    rightImageNumber = Random().nextInt(8) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          leftImageNumber == rightImageNumber ? 'MATCHED' : 'Try Again',
+          style: TextStyle(
+            fontSize: 42,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      ImageRandom();
+                    });
+                  },
+                  child: Image.asset('images/image-$leftImageNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      ImageRandom();
+                    });
+                  },
+                  child: Image.asset('images/image-$rightImageNumber.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
